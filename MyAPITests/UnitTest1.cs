5e351eb31fbc5f2c8608f430f102c26a398fcf8f
Jakob Wilson <jakobwwilson@gmail.com>
2020-08-03 15:55:57 +1000

    Initial Commit

---
+++ MyAPITests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyAPITests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
